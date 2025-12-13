69b81dd88f30c8bab71a31c46484ef8f719e4407
Danack <Danack@basereality.com>
2025-12-13 17:15:22 +0000

    Remote debugging.

--- runTests.sh
+++ runTests.sh
@@ -11,13 +11,28 @@ bash runCodeSniffer.sh
 echo "Running PHPStan"
 php vendor/bin/phpstan analyze -c ./phpstan.neon -l 8 src
 
-echo "Running Psalm"
-# The version of psalm we are using only works on below PHP 8.3
-if php -r 'exit(!(PHP_MAJOR_VERSION === 8 && (PHP_MINOR_VERSION === 1 || PHP_MINOR_VERSION === 2)));'; then
+echo "Running Psalm (debug enabled)"
+
+php -r '
+echo "PHP_MAJOR_VERSION=", PHP_MAJOR_VERSION, PHP_EOL;
+echo "PHP_MINOR_VERSION=", PHP_MINOR_VERSION, PHP_EOL;
+
+$ok = (PHP_MAJOR_VERSION === 8 && (PHP_MINOR_VERSION === 1 || PHP_MINOR_VERSION === 2));
+echo "Version check result: ", ($ok ? "PASS" : "FAIL"), PHP_EOL;
+
+exit(!$ok);
+'
+php_exit_code=$?
+
+echo "Version check exit code: $php_exit_code"
+
+if [ $php_exit_code -eq 0 ]; then
+    echo "Executing Psalm"
     php ./psalm.phar
 else
     echo "Skipping Psalm (requires PHP 8.1 or 8.2)"
 fi
+
 bash runExamples.sh
 
 echo ""
