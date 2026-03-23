72ee1fec2ffca440402f5bac212b09ec37b8d24d
Danack <Danack@basereality.com>
2026-03-23 20:15:37 +0000

    Move to more generic phpstan ignore

--- src/DataType/ProcessRule/EarlierThanParam.php
+++ src/DataType/ProcessRule/EarlierThanParam.php
@@ -83,7 +83,7 @@ class EarlierThanParam implements ProcessRule
             /** @var \DateTimeImmutable|\DateTime $previousValue */
             $timeToCompare = $previousValue->sub($timeOffset);
         }
-        // @phpstan-ignore catch.neverThrown
+        // @phpstan-ignore-next-line
         catch (\DateInvalidOperationException $dioe) {
             throw InvalidDatetimeFormatExceptionData::invalidTimeOffset(
                 "Time offset is invalid: " . $dioe->getMessage(),
